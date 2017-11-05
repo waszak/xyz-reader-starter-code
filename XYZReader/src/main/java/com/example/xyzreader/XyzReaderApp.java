eb91777b0658805edd2cc3ac875e73d516ad126e
waszak <kwaszak67@gmail.com>
2017-11-05 16:50:20 +0100

    feat:Support RTL

---
+++ XYZReader/src/main/java/com/example/xyzreader/XyzReaderApp.java
@@ -0,0 +1,16 @@
+package com.example.xyzreader;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class XyzReaderApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(BuildConfig.DEBUG){
+            Timber.uprootAll();
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
